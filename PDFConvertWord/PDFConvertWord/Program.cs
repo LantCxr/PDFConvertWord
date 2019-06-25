49c87ed02a6da705021f6ca89e149c71845cf319
LantCxr <525737396@qq.com>
2019-06-25 13:42:32 +0800

    提交

---
+++ PDFConvertWord/PDFConvertWord/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Spire.Pdf;
+
+namespace PDFConvertWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 转换
+
+            Console.WriteLine("开始转换.....");
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(@"F:\QQ下载文件\6秋培优.pdf");
+            Console.WriteLine("转换期间比较漫长请耐心等待.....");
+            doc.SaveToFile(@"F:\QQ下载文件\6秋培优.doc",
+                FileFormat.DOC);
+            Console.WriteLine("转换成功");
+            System.Diagnostics.Process.Start(@"F:\QQ下载文件\6秋培优.doc");
+
+            #endregion
+        }
+    }
+}
